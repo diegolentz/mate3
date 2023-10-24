bbbe17d15d2be7ae5dd69266616a1ae3a153aa0c
diegolentz <you@example.com>
2023-10-24 14:35:59 -0300

    agrega personas

--- persona.py
+++ persona.py
@@ -11,32 +11,26 @@ class Persona:
         self.mail_contacto = mail_contacto
         self.departamento = departamento
         self.dni = dni
-
+        
     def guardar_en_xlsx(self, nombre_archivo):
-        try:
-            # Abre el archivo Excel
+            # Abre el archivo Excel existente
             workbook = openpyxl.load_workbook(nombre_archivo)
-        except FileNotFoundError:
-            # Si el archivo no existe, se crea uno nuevo
-            workbook = openpyxl.Workbook()
+            # Selecciona la hoja de trabajo (worksheet) donde deseas escribir
+            worksheet = workbook.active
 
-        # Selecciona la hoja de trabajo (worksheet) donde deseas escribir o crear
-        worksheet = workbook.active
+            # Crear una lista de datos con los valores de la persona
+            datos = [self.nombre, self.apellido, self.ob_social, self.dx, self.urgencia, self.procedimiento, self.mail_contacto, self.departamento, self.dni]
 
-        # Crear una lista de datos con los valores de la persona
-        datos = [self.nombre, self.apellido, self.ob_social, self.dx, self.urgencia, self.procedimiento, self.mail_contacto, self.departamento, self.dni]
+            # Encuentra la primera fila vacía en la columna A
+            fila = 1
+            while worksheet.cell(row=fila, column=1).value:
+                fila += 1
 
-        # Agrega los encabezados a la primera fila si no existen
-        if worksheet.max_row == 1:
-            encabezados = ["NOMBRE", "APELLIDO", "OB. SOCIAL", "DIAGNOSTICO", "URGENCIA", "PROCEDIMIENTO", "MAIL", "DEPARTAMENTO", "DNI"]
-            worksheet.append(encabezados)
+            # Añade los datos a la fila encontrada
+            for col, dato in enumerate(datos, start=1):
+                worksheet.cell(row=fila, column=col, value=dato)
 
-        # Añade los datos a la segunda fila disponible en la hoja de trabajo
-        worksheet.append(datos)
-
-        # Guarda los cambios en el archivo Excel
-        workbook.save(nombre_archivo)
-
-# Ejemplo de uso
-nueva_persona = Persona("John", "Doe", "ObraSocial1", "DX1", "Urgencia", "Procedimiento1", "email@example.com", "Departamento1", "12345")
-nueva_persona.guardar_en_xlsx('clinicaDB.xlsx')
+            # Guarda los cambios en el archivo Excel
+            workbook.save(nombre_archivo)
+            # Cierra el archivo Excel
+            workbook.close()
