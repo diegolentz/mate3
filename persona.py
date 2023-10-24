e8906754631343a1f3b47926fb439ebb3623cc48
diegolentz <you@example.com>
2023-10-24 14:14:50 -0300

    arreglando menu

--- persona.py
+++ persona.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import openpyxl
 
 class Persona:
@@ -14,19 +13,30 @@ class Persona:
         self.dni = dni
 
     def guardar_en_xlsx(self, nombre_archivo):
-        # Abre el archivo Excel existente
-        workbook = openpyxl.load_workbook(nombre_archivo)
-        # Selecciona la hoja de trabajo (worksheet) donde deseas escribir
+        try:
+            # Abre el archivo Excel
+            workbook = openpyxl.load_workbook(nombre_archivo)
+        except FileNotFoundError:
+            # Si el archivo no existe, se crea uno nuevo
+            workbook = openpyxl.Workbook()
+
+        # Selecciona la hoja de trabajo (worksheet) donde deseas escribir o crear
         worksheet = workbook.active
 
         # Crear una lista de datos con los valores de la persona
         datos = [self.nombre, self.apellido, self.ob_social, self.dx, self.urgencia, self.procedimiento, self.mail_contacto, self.departamento, self.dni]
 
-        # Añade los datos a la siguiente fila disponible en la hoja de trabajo
-        fila = [str(dato) for dato in datos]
-        worksheet.append(fila)
+        # Agrega los encabezados a la primera fila si no existen
+        if worksheet.max_row == 1:
+            encabezados = ["NOMBRE", "APELLIDO", "OB. SOCIAL", "DIAGNOSTICO", "URGENCIA", "PROCEDIMIENTO", "MAIL", "DEPARTAMENTO", "DNI"]
+            worksheet.append(encabezados)
+
+        # Añade los datos a la segunda fila disponible en la hoja de trabajo
+        worksheet.append(datos)
 
         # Guarda los cambios en el archivo Excel
         workbook.save(nombre_archivo)
 
-
+# Ejemplo de uso
+nueva_persona = Persona("John", "Doe", "ObraSocial1", "DX1", "Urgencia", "Procedimiento1", "email@example.com", "Departamento1", "12345")
+nueva_persona.guardar_en_xlsx('clinicaDB.xlsx')
